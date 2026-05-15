d40d85c29ef61e61d5993efba4f0530b10ab23fa
Alex Sacara <alexsacara15@gmail.com>
2026-05-15 11:07:44 +0300

    Final version (Alex)

--- app/src/main/java/com/example/advancedcomputersciencecn6008_1/GeminiConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.advancedcomputersciencecn6008_1;
-
-public class GeminiConfig {
-    // Replace "YOUR_GEMINI_API_KEY" with your actual Gemini API key.
-    // Keep your API key secure and do not commit it to version control in production apps.
-    public static final String GEMINI_API_KEY = "AIzaSyA0v8f9_zoEJqT8b5vX77hII5AnmDO8gqg";
-}
